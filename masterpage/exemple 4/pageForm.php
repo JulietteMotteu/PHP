d2781cee54687669008304052b1a2b611da06f92
Juliette Motteu <juliettemotteu@gmail.com>
2018-10-26 14:40:08 +0200

    masterpage

---
+++ masterpage/exemple 4/pageForm.php
@@ -0,0 +1,11 @@
+<p>
+    Je contiens un form
+    
+</p>
+
+<form action="./index.php?p=pageFormTraitement" method="post">
+    
+    <input type="text" name="nom">
+    <button>Envoyer</button>
+    
+</form>
---
+++ masterpage/exemple 4/pageFormTraitement.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Je suis la page de traitement<br>";
+
+echo 'Salut ' . $_POST['nom'];
+
+?>
