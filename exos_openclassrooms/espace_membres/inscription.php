989c5d677a82df960f9a588088334f18c380166b
Juliette Motteu <juliettemotteu@gmail.com>
2019-01-16 10:55:11 +0100

    inscription oc

--- exos_openclassrooms/espace_membres/inscription.php
+++ exos_openclassrooms/espace_membres/inscription.php
@@ -6,12 +6,24 @@ catch (Exception $e) {
     die ('Erreur : ' . $e->getMessage());
 }
 
+// Regex Email
+$regexEmail = "#^[a-z0-9.-_]+@[a-z0-9.-_]{2,}\.[a-z]{2,4}$#";
+
 // Vérification de la validité des informations
-if(isset($_POST['password'])) {
-    $statement = $bdd->prepare('SELECT pseudo FROM membres WHERE pass = :password');
-    $statement->bindValue(':password', $_POST['password'], PDO::PARAM_STR);
+if(isset($_POST['pseudo'], $_POST['password'], $_POST['passwordConfirm'], $_POST['email'])) {
+    $statement = $bdd->prepare('SELECT pseudo FROM membres WHERE UPPER(pseudo) = UPPER(:pseudo)');
+    $statement->bindValue(':pseudo', $_POST['pseudo'], PDO::PARAM_STR);
     $statement->execute();
-    if()
+    $pseudoVerify = $statement->fetchAll();
+    
+    $statement->closeCursor();
+    
+    if(empty($pseudoVerify && $_POST['password'] == $_POST['passwordConfirm']) && preg_match($regexEmail, $_POST['mail'])) {
+        $statement = $bdd->prepare('INSERT INTO membres(pseudo, pass, email, date_inscription) VALUES(:pseudo, :pass, :email, CURDATE())');
+        $statement->bindValue(':pseudo', htmlspecialchars($_POST['pseudo']), PDO::PARAM_STR);
+        $statement->bindValue(':pass', htmlspecialchars($_POST['password']), PDO::PARAM_STR);
+        $statement->bindValue(':email', htmlspecialchars($_POST['email']), PDO::PARAM_STR);
+    }
 }
 
 // Hachage du mot de passe
@@ -75,7 +87,7 @@ $req->execute(array(
            <label for="email">Email</label>
            <input type="text" name="email">
        </div>
-       
+       <button type="submit">Inscription</button>
    </form>
     
 </body>
